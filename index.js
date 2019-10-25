ca0de07fbd50739553edc517b90d541c72807c6d
Ted Ogmundson <togmund@gmail.com>
2019-10-25 22:44:08 +0000

    moved functions into methods inside the object

--- index.js
+++ index.js
@@ -8,32 +8,32 @@ function newTask(title, description) {
   const task = {
     title: title,
     description: description,
-    complete: false
-  }
+    complete: false,
+
+    logState: function() {
+      console.log(`${this.title} has${this.complete ? " " : " not "}been completed`);
+    },
+
+    markCompleted: function() {
+      this.complete = true;
+    }
+  };
   return task;
-  // taskTitles.push(title);
-  // taskDescriptions.push(description)
-  // taskComplete.push(false);
 }
 
 // Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
-function completeTask(task) {
-  console.log(`${task.title} has${task.complete ? " " : " not "}been completed`);
-}
+
 
 // Print the state of a task to the console in a nice readable way
-function logTaskState(task) {
-  task.complete = true
-}
+
 
 // DRIVER CODE BELOW
 const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box");
 const task2 = newTask("Do Laundry", "😨");
 const tasks = [task1, task2];
 
-// for now, let's just make sure we see our tasks
 console.log(tasks);
-logTaskState(task1); // Clean Cat Litter has not been completed
-completeTask(task1);
-logTaskState(task1); // Clean Cat Litter has been completed
+task1.logState(); // Clean Cat Litter has not been completed
+task1.markCompleted();
+task1.logState(); // Clean Cat Li
 console.log(tasks);
